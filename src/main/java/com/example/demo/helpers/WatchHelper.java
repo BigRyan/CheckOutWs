47c3450d410bb86c029fae1265caf70770f8737f
任亮 <renliang@renliangdeMacBook-Pro.local>
2019-09-08 22:00:11 +0800

    Add Checkout logic and test

---
+++ src/main/java/com/example/demo/helpers/WatchHelper.java
@@ -0,0 +1,7 @@
+package com.example.demo.helpers;
+
+/**
+ * Created by renliang on 2019/9/8.
+ */
+public class WatchHelper {
+}
--- src/test/java/com/example/demo/integrations/DemoApplicationTests.java
+++ src/test/java/com/example/demo/integrations/DemoApplicationTests.java
@@ -1,14 +1,18 @@
-package com.example.demo;
+package com.example.demo.integrations;
 
+import com.example.demo.model.Watch;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.ws.rs.core.MediaType;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -22,14 +26,25 @@ public class DemoApplicationTests {
 
 	@Test
 	public void testCheckout() {
-		String url = "endpoint url";
-		String requestJson = "{\"001\",:\"002\",:\"003\"}";
-		HttpEntity<String> request_entity = new HttpEntity<String>(requestJson);
+		String requestJson = "[\"001\",\"001\",\"001\", \"003\"]";
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-Type", MediaType.APPLICATION_JSON);
+		HttpEntity<String> request_entity = new HttpEntity<String>(requestJson,headers);
+
 		ResponseEntity<String> response_entity = this.restTemplate
 				.postForEntity("/checkout", request_entity, String.class);
 
 		assertThat(response_entity.getStatusCode()).isEqualTo(HttpStatus.OK);
-		assertThat(response_entity.getBody()).isEqualTo("{\"price\":\"12345\"}");
+		assertThat(response_entity.getBody()).isEqualTo("{\"price\":\"250\"}");
+	}
+
+	@Test
+	public void testFindWatchByNo() {
+		ResponseEntity<Watch> response_entity = this.restTemplate
+				.getForEntity("/watches/001",  Watch.class);
+		Watch watch = response_entity.getBody();
+		assertThat(response_entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(watch.getWatchNo()).isEqualTo("001");
 	}
 
 }
